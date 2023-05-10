a54c8592a715e5804758c5b6b3326747f91bb4a6
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2023-05-10 23:06:03 +0200

    initial

--- train_regression.sh
+++ train_regression.sh
@@ -1,2 +1,2 @@
-#python train_regression.py  --epochs 500 --config regressJet_onlyR --prefix onlyR 
-python train_regression.py  --epochs 500 --config regressJet_onlyGamma --prefix onlyGamma
+python train_regression.py  --epochs 500 --config regressJet_onlyR --prefix onlyR 
+#python train_regression.py  --epochs 500 --config regressJet_onlyGamma --prefix onlyGamma
