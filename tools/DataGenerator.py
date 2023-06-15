0c257d77563304a7102180bd73eb1ddd2112655d
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2023-06-15 21:01:30 +0200

    fix dataloadr & locations

--- tools/DataGenerator.py
+++ tools/DataGenerator.py
@@ -11,7 +11,7 @@ import awkward as ak
 import numpy as np
 import os
 
-def chunk( tot, n_split, index):
+def get_chunk( tot, n_split, index):
     ''' Implements split of number into n_split chunks
         https://math.stackexchange.com/questions/2975936/split-a-number-into-n-numbers
         Return tuple (start, stop)
@@ -37,6 +37,7 @@ class DataGenerator(Sequence):
             splitting_strategy  = "files",
             tree_name           = "Events",
             selection           = None,
+            max_files           = None,
                 ):
         '''
         DataGenerator for the keras training framework.
@@ -58,6 +59,8 @@ class DataGenerator(Sequence):
             else:
                 raise RuntimeError( "Don't know what to do with %r" % filename )
 
+        self.input_files = self.input_files[:max_files]
+
         self.splitting_strategy = splitting_strategy
         if splitting_strategy.lower() not in ['files', 'events']:
             raise RuntimeError("'splitting_strategy' must be 'files' or 'events'")
@@ -78,7 +81,7 @@ class DataGenerator(Sequence):
         self.branches = branches 
 
         # recall the index we loaded
-        self.index          = -1
+        self.index          = None
 
         # name of the tree to be read
         self.tree_name = tree_name
@@ -87,8 +90,9 @@ class DataGenerator(Sequence):
     def __len__( self ):
         return self.n_split
 
-    def load( self, index = -1, small = None):
+    def _load( self, index, small = None):
 
+        print ("Loading index %i, strategy: %s"%(index, self.splitting_strategy.lower() ) )
         if index>=0:
             n_split = self.n_split
         else:
@@ -97,33 +101,27 @@ class DataGenerator(Sequence):
 
         # load the files (don't load the data)
         if self.splitting_strategy.lower() == 'files':
-            filestart, filestop = chunk( len(self.input_files), n_split, index )
-        else:
-            filestart, filestop = 0, len(self.input_files)
-
-
-        array = uproot.concatenate([f+':'+self.tree_name for f in self.input_files], self.branches)
-        print(array)
-        # apply selection, if any
-        len_before = len(array)
-        if self.selection is not None: 
-            array = array[self.selection(array)]
-            print ("Applying selection with efficiency %4.3f" % (len(array)/len_before) )
-
-        if self.splitting_strategy.lower() == 'events':
-            entry_start, entry_stop = chunk( len(array), n_split, index )
-        else:
-            entry_start, entry_stop = 0, len(array)
+            filestart, filestop = get_chunk( len(self.input_files), n_split, index )
+            self.array          = uproot.concatenate([f+':'+self.tree_name for f in self.input_files[filestart:filestop]], self.branches)
+            if self.selection is not None:
+                len_before = len(self.array)
+                self.array = self.array[self.selection(self.array)]
+                print ("Applying selection with efficiency %4.3f" % (len(array)/len_before) )
+            entry_start, entry_stop = 0, len(self.array)
+        elif self.splitting_strategy.lower() == 'events':
+            if not hasattr( self, "array" ):
+                self.array      = uproot.concatenate([f+':'+self.tree_name for f in self.input_files], self.branches)
+                if self.selection is not None:
+                    len_before = len(self.array)
+                    self.array = self.array[self.selection(self.array)]
+                    print ("Applying selection with efficiency %4.3f" % (len(array)/len_before) )
+            entry_start, entry_stop = get_chunk( len(self.array), n_split, index )
 
         if small is not None and small>0:
             entry_stop = min( entry_stop, entry_start+small )
 
         self.index = index
-
-        if self.selection is not None: 
-            self.data = array[entry_start:entry_stop]
-        else:
-            self.data = array[entry_start:entry_stop]
+        self.data  = self.array[entry_start:entry_stop]
 
         return self.data
 
@@ -131,7 +129,7 @@ class DataGenerator(Sequence):
         if index == self.index:
             return self.data
         else:
-            return self.load( index )
+            return self._load( index )
 
     def scalar_branches( self, branches ):
     
@@ -150,11 +148,34 @@ class DataGenerator(Sequence):
             return np.array([ np.array(ak.fill_none(ak.pad_none(self.data[b].to_list(), target=padding_target, clip=True), value=padding_value)).transpose() for b in branches ]).transpose()
 
 if __name__=='__main__':
+    import user
+    path = os.path.join(user.data_directory, "v6/WZto1L1Nu_HT300")
 
     data = DataGenerator(
-        input_files = ["/groups/hephy/cms/robert.schoefbeck/TMB/postprocessed/gen/v2/tschRefPointNoWidthRW/"],
-            n_split = 1,
+            input_files = [path],
+            n_split   = -1,
             splitting_strategy = "files",
-            branches = ["genJet_pt", "genJet_eta", "nchh"], 
-        ) 
+            branches  = ["genJet_pt", "genJet_eta"],
+            max_files = 10, 
+        )
+    total = 0
+    for i_chunk, chunk in enumerate(data):
+        total += len(chunk)
+        print ("Loaded chunk %i with %i events."%(i_chunk, len(chunk)))
+    print ("Total:", total )
 
+    data = DataGenerator(
+            input_files = [path],
+            n_split   = 10,
+            splitting_strategy = "events",
+            branches  = ["genJet_pt", "genJet_eta"],
+            max_files = 10, 
+        )
+
+    print()
+
+    total = 0
+    for i_chunk, chunk in enumerate(data):
+        total += len(chunk)
+        print ("Loaded chunk %i with %i events."%(i_chunk, len(chunk)))
+    print ("Total:", total )
--- tools/user.py
+++ tools/user.py
@@ -3,28 +3,12 @@ import os
 if os.environ['USER'] in ['robert.schoefbeck']:
     plot_directory         = "/groups/hephy/cms/robert.schoefbeck/www/pytorch/"
     model_directory        = "/groups/hephy/cms/robert.schoefbeck/NN/models/"
-    data_directory         = "/groups/hephy/cms/robert.schoefbeck/NN/data/"
-elif os.environ['USER'] in ['oskar.rothbacher']:
-    plot_directory         = "/groups/hephy/cms/oskar.rothbacher/www/pytorch/"
-    model_directory        = "/groups/hephy/cms/oskar.rothbacher/NN/models/"
-    data_directory         = "/groups/hephy/cms/oskar.rothbacher/NN/data/"
-elif os.environ['USER'] in ['dietrich.liko']:
-    plot_directory         = "/groups/hephy/cms/dietrich.liko/www/pytorch/"
-    model_directory        = "/groups/hephy/cms/dietrich.liko/NN/models/"
-    data_directory         = "/groups/hephy/cms/dietrich.liko/NN/data/"
-elif os.environ['USER'] in ['robert.schoefbeckcern.ch']:
-    plot_directory = './plots/'
-    model_directory= "/Users/robert.schoefbeckcern.ch/ML-pytorch/models"
-    data_directory = "/Users/robert.schoefbeckcern.ch/ML-pytorch/data"
-elif os.environ['USER'] in ['lena.wild']:
-    plot_directory = './plots/'
-    #plot_directory         = "/groups/hephy/cms/lena.wild/www/pytorch/"
-    model_directory        = "/groups/hephy/cms/lena.wild/NN/models/"
-    data_directory         = "/groups/hephy/cms/lena.wild/NN/data/"
+    data_directory         = "/scratch-cbe/users/robert.schoefbeck/HadronicSMEFT/postprocessed/gen/"
 elif os.environ['USER'] in ['suman.chatterjee']:
     plot_directory = './plots/'
     model_directory= "/groups/hephy/cms/suman.chatterjee/ML-pytorch/models/"
-    data_directory = "groups/hephy/cms/suman.chatterjee/ML-pytorch/data/"
+    #data_directory = "groups/hephy/cms/suman.chatterjee/ML-pytorch/data/"
+    data_directory         = "/scratch-cbe/users/robert.schoefbeck/HadronicSMEFT/postprocessed/gen/"
 elif os.environ['USER'] in ['sesanche']:
     plot_directory = './plots/'
     model_directory= "./models/"
