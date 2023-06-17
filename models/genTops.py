6bdb7771c501aa4938fda8abdfdf354d8e149808
Robert Schoefbeck <robert.schoefbeck@cern.ch>
2023-06-17 19:39:29 +0200

    update

--- models/genTops.py
+++ models/genTops.py
@@ -10,6 +10,9 @@ if __name__=="__main__":
 
 from tools.DataGenerator import DataGenerator
 from tools.WeightInfo    import WeightInfo
+import torch
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 selection = lambda ar: (ar.genJet_pt>500) & (ar.genJet_SDmass>0) & (abs(ar.dR_genJet_maxq1q2b)<0.6) & (ar.genJet_SDsubjet1_mass>=0)
 # -> https://schoef.web.cern.ch/schoef/pytorch/choleskyNN/genTops/training_plots/choleskyNN_genTops_ctWRe_nTraining_519075/lin/epoch.gif
@@ -21,20 +24,31 @@ data_generator =  DataGenerator(
         splitting_strategy = "files",
         selection   = selection,
         branches = [
-                    #"genQ1_pt", "genQ1_eta", "genQ1_phi", "genQ1_mass", "genQ1_pdgId", "genQ2_pt", "genQ2_eta", "genQ2_phi", "genQ2_mass", "genQ2_pdgId", 
-                    #"genb_pt", "genb_eta", "genb_phi", "genb_mass", "genb_pdgId", 
-                    #"genW_pt", "genW_eta", "genW_phi", "genW_mass", "genJet_pt", "genJet_eta", "genJet_phi", "genJet_mass", "genJet_nConstituents", 
-                    #"genJet_SDmass", "genJet_SDsubjet0_eta", "genJet_SDsubjet0_deltaEta", "genJet_SDsubjet0_phi", "genJet_SDsubjet0_deltaPhi", "genJet_SDsubjet0_deltaR", 
-                    #"genJet_SDsubjet0_mass", "genJet_SDsubjet1_eta", "genJet_SDsubjet1_deltaEta", "genJet_SDsubjet1_phi", "genJet_SDsubjet1_deltaPhi", 
-                    #"genJet_SDsubjet1_deltaR", "genJet_SDsubjet1_mass", 
-                    #"genJet_tau1", "genJet_tau2", "genJet_tau3", "genJet_tau4", "genJet_tau21", "genJet_tau32", 
-                    #"genJet_ecf1", "genJet_ecf2", "genJet_ecf3", "genJet_ecfC1", "genJet_ecfC2", "genJet_ecfC3", "genJet_ecfD", "genJet_ecfDbeta2", "genJet_ecfM1", "genJet_ecfM2", "genJet_ecfM3", "genJet_ecfM1beta2", "genJet_ecfM2beta2", "genJet_ecfM3beta2", "genJet_ecfN1", "genJet_ecfN2", "genJet_ecfN3", "genJet_ecfN1beta2", "genJet_ecfN2beta2", "genJet_ecfN3beta2", "genJet_ecfU1", "genJet_ecfU2", "genJet_ecfU3", "genJet_ecfU1beta2", "genJet_ecfU2beta2", "genJet_ecfU3beta2", 
-                    #"dR_genJet_Q1", "dR_genJet_Q2", "dR_genJet_W", "dR_genJet_b", "dR_genJet_top", "dR_genJet_maxQ1Q2b",
-                    "genJet_pt", "genJet_SDmass", "dR_genJet_maxq1q2b", "genJet_SDsubjet1_mass", 
+            "genJet_pt", "dR_genJet_maxq1q2b", "genJet_mass", "genJet_nConstituents", "genJet_SDmass", "genJet_SDsubjet0_deltaEta", "genJet_SDsubjet0_deltaPhi", "genJet_SDsubjet0_deltaR", "genJet_SDsubjet0_mass", "genJet_SDsubjet1_deltaEta", "genJet_SDsubjet1_deltaPhi", "genJet_SDsubjet1_deltaR", "genJet_SDsubjet1_mass", "genJet_tau1", "genJet_tau2", "genJet_tau3", "genJet_tau4", "genJet_tau21", "genJet_tau32", "genJet_ecf1", "genJet_ecf2", "genJet_ecf3", "genJet_ecfC1", "genJet_ecfC2", "genJet_ecfC3", "genJet_ecfD", "genJet_ecfDbeta2", "genJet_ecfM1", "genJet_ecfM2", "genJet_ecfM3", "genJet_ecfM1beta2", "genJet_ecfM2beta2", "genJet_ecfM3beta2", "genJet_ecfN1", "genJet_ecfN2", "genJet_ecfN3", "genJet_ecfN1beta2", "genJet_ecfN2beta2", "genJet_ecfN3beta2", "genJet_ecfU1", "genJet_ecfU2", "genJet_ecfU3", "genJet_ecfU1beta2", "genJet_ecfU2beta2", "genJet_ecfU3beta2", 
 
-                    "ngen", "gen_pt", "gen_etarel", "gen_phirel", "gen_eta", "gen_phi", "gen_pdgId", "gen_charge", "gen_type",
-                    "ctWRe_coeff", "p_C"]
+        "ngen", "gen_pt", "gen_etarel", "gen_phirel", "gen_eta", "gen_phi", "gen_pdgId", "gen_charge", "gen_type",
+        "ctWRe_coeff", "p_C", "ctWRe_coeff"]
     )
+
+class genTopsModel:
+    def __init__( self ):
+        self.data_generator = data_generator
+ 
+    @staticmethod
+    def getEvents(data):
+        padding = 200 
+        ctwRe_coeff = DataGenerator.vector_branch( data, 'ctWRe_coeff',padding_target=3 )
+        weights = ctwRe_coeff[:, 0]
+        truth = ctwRe_coeff[:, 1]/ctwRe_coeff[:, 0]
+
+        pts = DataGenerator.vector_branch( data, 'gen_pt',padding_target=padding )
+        ptmask = torch.ones_like( torch.Tensor(pts) ).to(device) #  (pts > 5)
+        detas = DataGenerator.vector_branch( data, 'gen_etarel',padding_target=padding ) # [ptmask]
+        dphis = DataGenerator.vector_branch( data, 'gen_phirel',padding_target=padding ) # [ptmask
+        detas = torch.Tensor(detas).to(device) * ptmask  # 0-pad the pt < 5
+        pts   = torch.Tensor(pts).to(device)   * ptmask  # 0-pad the pt < 5
+        dphis = torch.Tensor(dphis).to(device) * ptmask  # 0-pad the pt < 5
+        return pts, torch.stack( (dphis, detas), axis=-1), torch.tensor(weights).to(device), torch.tensor(truth).to(device)
 
 reweight_pkl = '/eos/vbc/group/cms/robert.schoefbeck/gridpacks/ParticleNet/t-sch-RefPoint-noWidthRW_reweight_card.pkl'
 weightInfo = WeightInfo(reweight_pkl)
@@ -59,14 +73,8 @@ else:
 
 vector_branches = ["gen_pt", "gen_etarel", "gen_phirel"]
 
-feature_names = [   #"genJet_pt", "genJet_mass", "genJet_nConstituents",
-                    #"genJet_SDmass", "genJet_SDsubjet0_deltaEta", "genJet_SDsubjet0_deltaPhi", "genJet_SDsubjet0_deltaR",
-                    #"genJet_SDsubjet0_mass", "genJet_SDsubjet1_deltaEta", "genJet_SDsubjet1_deltaPhi",
-                    #"genJet_SDsubjet1_deltaR", "genJet_SDsubjet1_mass",
-                    #"genJet_tau1", "genJet_tau2", "genJet_tau3", "genJet_tau4", "genJet_tau21", "genJet_tau32", 
-                    #"genJet_ecf1", "genJet_ecf2", "genJet_ecf3", "genJet_ecfC1", "genJet_ecfC2", "genJet_ecfC3", "genJet_ecfD", "genJet_ecfDbeta2", "genJet_ecfM1", "genJet_ecfM2", "genJet_ecfM3", "genJet_ecfM1beta2", "genJet_ecfM2beta2", "genJet_ecfM3beta2", "genJet_ecfN1", "genJet_ecfN2", "genJet_ecfN3", "genJet_ecfN1beta2", "genJet_ecfN2beta2", "genJet_ecfN3beta2", "genJet_ecfU1", "genJet_ecfU2", "genJet_ecfU3", "genJet_ecfU1beta2", "genJet_ecfU2beta2", "genJet_ecfU3beta2",
-
-                    "genJet_pt", "genJet_SDmass", "dR_genJet_maxq1q2b", "genJet_SDsubjet1_mass", 
+feature_names = [   
+    "genJet_pt", "dR_genJet_maxq1q2b", "genJet_mass", "genJet_nConstituents", "genJet_SDmass", "genJet_SDsubjet0_deltaEta", "genJet_SDsubjet0_deltaPhi", "genJet_SDsubjet0_deltaR", "genJet_SDsubjet0_mass", "genJet_SDsubjet1_deltaEta", "genJet_SDsubjet1_deltaPhi", "genJet_SDsubjet1_deltaR", "genJet_SDsubjet1_mass", "genJet_tau1", "genJet_tau2", "genJet_tau3", "genJet_tau4", "genJet_tau21", "genJet_tau32", "genJet_ecf1", "genJet_ecf2", "genJet_ecf3", "genJet_ecfC1", "genJet_ecfC2", "genJet_ecfC3", "genJet_ecfD", "genJet_ecfDbeta2", "genJet_ecfM1", "genJet_ecfM2", "genJet_ecfM3", "genJet_ecfM1beta2", "genJet_ecfM2beta2", "genJet_ecfM3beta2", "genJet_ecfN1", "genJet_ecfN2", "genJet_ecfN3", "genJet_ecfN1beta2", "genJet_ecfN2beta2", "genJet_ecfN3beta2", "genJet_ecfU1", "genJet_ecfU2", "genJet_ecfU3", "genJet_ecfU1beta2", "genJet_ecfU2beta2", "genJet_ecfU3beta2", 
                     ]
 
 def make_combinations( coefficients ):
@@ -89,20 +97,21 @@ def getEvents( data ):
 tex = {"ctWRe":"C_{tW}^{Re}", "ctWIm":"C_{tW}^{Im}", "ctBIm":"C_{tB}^{Im}", "ctBRe":"C_{tB}^{Re}", "cHt":"C_{Ht}", 'cHtbRe':'C_{Htb}^{Re}', 'cHtbIm':'C_{Htb}^{Im}', 'cHQ3':'C_{HQ}^{(3)}'}
 
 #['ctWRe', 'ctBRe', 'cHQ3', 'cHt', 'cHtbRe', 'ctWIm', 'ctBIm', 'cHtbIm']
-
 eft_plot_points = [
     {'color':ROOT.kBlack,       'eft':sm, 'tex':"SM"},
-    {'color':ROOT.kMagenta-4,   'eft':make_eft(ctWRe=3),   'tex':"c_{tW}^{Re}=3",   },
-    {'color':ROOT.kMagenta+2,   'eft':make_eft(ctWIm=5),   'tex':"c_{tW}^{Im}=5",   },
-    {'color':ROOT.kGreen-4,     'eft':make_eft(cHtbRe=5),  'tex':"c_{Htb}^{Re}=5",  },
-    {'color':ROOT.kGreen+2,     'eft':make_eft(cHtbIm=5),  'tex':"c_{Htb}^{Im}=5",  },
-    {'color':ROOT.kBlue+2,      'eft':make_eft(cHQ3=5),    'tex':"c_{HQ}^{(3)}=5",  },
+    {'color':ROOT.kMagenta-4,   'eft':make_eft(ctWRe=1),   'tex':"c_{tW}^{Re}=1",},
+    {'color':ROOT.kMagenta+2,   'eft':make_eft(ctWRe=-1),   'tex':"c_{tW}^{Re}=-1",},
+    {'color':ROOT.kGreen-4,     'eft':make_eft(ctWRe=3),   'tex':"c_{tW}^{Re}=3",},
+    {'color':ROOT.kGreen+2,     'eft':make_eft(ctWRe=-3),   'tex':"c_{tW}^{Re}=-3",},
+    {'color':ROOT.kBlue+2,      'eft':make_eft(ctWRe=5),   'tex':"c_{tW}^{Re}=5",},
+    {'color':ROOT.kBlue-4,      'eft':make_eft(ctWRe=-5),   'tex':"c_{tW}^{Re}=-5",},
     ]
 plot_options =  {
     "genJet_pt"                 :{'binning':[50,500,2000], 'tex':'p_{T}(jet)'},
     "genJet_mass"               :{'binning':[50,150,200], 'tex':'M(jet) unpruned'},
     "genJet_nConstituents"      :{'binning':[50,30,230], 'tex':'n-constituents'},
     "genJet_SDmass"             :{'binning':[50,150,200], 'tex':'M_{SD}(jet)'},
+    "dR_genJet_maxq1q2b"        :{'binning':[50,0,1], 'tex':'max #Delta R(jet, q1/q2/b})'},
     "genJet_SDsubjet0_deltaEta" :{'binning':[50,-0.6,0.6], 'tex':'#Delta#eta(jet,jet_{SD,0})'},
     "genJet_SDsubjet0_deltaPhi" :{'binning':[50,-0.6,0.6], 'tex':'#Delta#phi(jet,jet_{SD,0})'},
     "genJet_SDsubjet0_deltaR"   :{'binning':[50,0,0.7], 'tex':'#Delta R(jet,jet_{SD,0})'},
@@ -146,60 +155,8 @@ plot_options =  {
     "genJet_ecfU3beta2"         :{'binning':[50,0,0.004], 'tex':"ecfU3beta2"},
 }
 
-multi_bit_cfg = {'n_trees': 100,
-                 'max_depth': 4,
-                 'learning_rate': 0.20,
-                 'min_size': 15 }
-
 if __name__=="__main__":
    
     # load some events and their weights 
     x, w = getEvents(data_generator[0])
 
-    # x are a list of feature-vectors such that x[0] are the features of the first event. Their branch-names are stored in feature_names.
-    # w are a dictionary with the weight-coefficients. The key tuple(), i.e., the empty n-tuple, is the constant term. The key ('ctWRe', ), i.e., the coefficient 
-    # that is an tuple of length one is the linear derivative (in this case wrt to ctWRe). The quadratic derivatives are stored in the keys ('ctWRe', 'ctWRe') etc.
-    # The list of Wilson coefficients is in: weightInfo.variables
-    # The list of all derivatives (i.e., the list of all combiations of all variables up to length 2) is weightInfo.combinations. It includes the zeroth derivative, i.e., the constant term.
-
-    # Let us scale all the weights to reasonable numbers. They come out of MG very small because the cross section of the process I used to genereate the top quarks is so small: s-channel single-top
-    # Let us add up the constant terms of all events and normalize the sample to the number of events. (Arbitrary choice)
-    const = (len(w[()])/w[()].sum())
-    for k in w.keys():
-        w[k] *= const 
-
-    ##let's remove the most extreme weight derivatives ... cosmetics for the propaganda plots
-    #from   tools import helpers 
-    #len_before = len(x)
-    #auto_clip = 0.001
-    #x, w = helpers.clip_quantile(x, auto_clip, weights = w )
-    #print ("Auto clip efficiency (training) %4.3f is %4.3f"%( auto_clip, len(x)/len_before ) )
-
-    print ("Wilson coefficients:", weightInfo.variables )
-    #print ("Features of the first event:\n" + "\n".join( ["%25s = %4.3f"%(name, value) for name, value in zip(feature_names, x[0])] ) )
-    prstr = {0:'constant', 1:'linear', 2:'quadratic'}
-    print ("Weight coefficients(!) of the first event:\n"+"\n".join( ["%30s = %4.3E"%( prstr[len(comb)] + " " +",".join(comb), w[comb][0]) for comb in weightInfo.combinations] ) )
-
-    # Let us compute the quadratic weight for ctWRe=1:
-    import copy
-    eft_sm  = make_eft()
-    eft_bsm = make_eft(ctWRe=1)
-    # constant term
-    reweight = copy.deepcopy(w[()])
-    # linear term
-    for param1 in wilson_coefficients:
-        reweight += (eft_bsm[param1]-eft_sm[param1])*w[(param1,)] 
-    # quadratic term
-    for param1 in wilson_coefficients:
-        if eft_bsm[param1]-eft_sm[param1] ==0: continue
-        for param2 in wilson_coefficients:
-            if eft_bsm[param2]-eft_sm[param2] ==0: continue
-            reweight += .5*(eft_bsm[param1]-eft_sm[param1])*(eft_bsm[param2]-eft_sm[param2])*w[tuple(sorted((param1,param2)))]
-
-    print ("w(ctWRe=1) for the first event is: ", reweight[0])
-
-    # Let us compute the weight ratio we can use for the training:
-    target_ctWRe        = w[('ctWRe',)]/w[()]
-    target_ctWRe_ctWRe  = w[('ctWRe','ctWRe')]/w[()]
-
-    # NOTE!! These "target" branches are already written to the training data! No need to compute
