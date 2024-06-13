c2015e7561eeaf76977de1f1c4af2d54830862f6
Sergio Sanchez Cruz <s.scruz.312@gmail.com>
2024-06-13 10:13:56 +0200

    adding paper configs

---
+++ configs/regress_wz_eft_new_cW.py
@@ -0,0 +1,103 @@
+import warnings
+warnings.filterwarnings("ignore")
+
+import os
+import math
+import numpy as np
+import torch
+import pickle
+import glob
+import itertools
+import operator
+import functools
+import ROOT 
+import tools.helpers as helpers
+ROOT.gROOT.SetBatch(True)
+
+from models.WZModel import WZModel
+import matplotlib.pyplot as plt 
+
+import SMEFTNet
+#import sys
+#sys.path.insert(0, '..')
+#sys.path.insert(0, '../..')
+#sys.path.insert(0, '../../..')
+import tools.user as user
+
+conv_params     = ( [(0.0, [20, 20])] )
+readout_params  = (0.0, [32, 32])
+dRN = 0.4
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+scalar_features = ["genJet_pt",'parton_lepV_pt']
+
+data_model_train = WZModel(what='VV_eflow', scalar_features = scalar_features, operator='cW', nsplit=500, train=True , linear_term_range=[-1000,1000])
+data_model_test  = WZModel(what='VV_eflow', scalar_features = scalar_features, operator='cW', nsplit=500, train=False, linear_term_range=[-1000,1000])
+
+
+from SMEFTNet import SMEFTNet
+model = SMEFTNet(
+    dRN=dRN,
+    conv_params=conv_params,
+    readout_params=readout_params,
+    learn_from_gamma=True,
+    num_scalar_features=len(scalar_features),
+    num_classes = 1,
+    regression=True,
+   ).to(device)
+
+
+def loss( out, truth, weights=None):
+    weight_sm = weights[:,0]
+    target    = weights[:,1]
+    return torch.mean( weight_sm*( out[:,0] - target )**2)
+
+truth_var_names = ['phi', 'hadV_pt', 'lepV_pt']
+ranges = [(-math.pi, math.pi, 50), (300, 1000,20), (0,1000,20)]
+def plot_chunk( out, truth, weights,  scalar_features=None, chunk_number=0):
+    weight_sm = weights[:,0]
+    target    = weights[:,1]
+
+    ret={}
+    for var, (varname, (xlow,xhigh, bins)) in enumerate(zip(truth_var_names,ranges)):
+        score = ROOT.TH1F("score_%s_%d"%(varname,chunk_number) , "score", bins, xlow, xhigh)
+        pred  = ROOT.TH1F("pred_%s_%d" %(varname,chunk_number) , "pred" , bins, xlow, xhigh)
+        norm  = ROOT.TH1F("norm_%s_%d" %(varname,chunk_number) , "norm" , bins, xlow, xhigh)
+
+        
+        score.Add( helpers.make_TH1F( np.histogram(truth[:,var], np.linspace(xlow, xhigh, bins+1), weights=weight_sm*target) ))
+        pred .Add( helpers.make_TH1F( np.histogram(truth[:,var], np.linspace(xlow, xhigh, bins+1), weights=weight_sm*out[:,0]) ))
+        norm .Add( helpers.make_TH1F( np.histogram(truth[:,var], np.linspace(xlow, xhigh, bins+1), weights=weight_sm)) )
+        ret[varname]=[score, pred, norm]
+    return ret
+
+def add_chunks( chunks ):
+    ret={}
+    for var in chunks[0]:
+        ret[var]=chunks[0][var]
+        for i in range(1,len(chunks)):
+            for j in range(3):
+                ret[var][j].Add(chunks[i][var][j])
+    return ret
+
+def save( objects, model_directory, epoch ):
+
+    outf=ROOT.TFile.Open(os.path.join( model_directory, f'test_closure_{epoch}.root'), "recreate")
+    for varname in objects:
+        score,pred,norm=objects[varname]
+    
+        score.Divide(norm)
+        pred.Divide(norm)
+
+        c1 = ROOT.TCanvas()
+        score.SetLineStyle(2)
+        score.GetXaxis().SetTitle(varname)
+        score.Draw()
+        pred.Draw("same")
+        c1.Print( '%s/closure_%s_%03i.png'%(model_directory,varname,epoch))
+        
+        outf.WriteTObject( score, "true_%s"%varname)
+        outf.WriteTObject( pred , "surrogate_%s"%varname)
+
+    outf.Close()
--- sergio/train_regression.py
+++ sergio/train_regression.py
@@ -22,7 +22,6 @@ args = parser.parse_args()
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if not torch.cuda.is_available():
-    print("Hola")
     torch.set_num_threads(16)
 
 import sys
@@ -79,68 +78,57 @@ if not args.overwrite:
 
 for epoch in range(epoch_min, args.epochs):
 
-    #if epoch%10==0 or epoch==epoch_min:
-    #    train_mask = torch.FloatTensor(args.nTraining).uniform_() < 0.8
-    #    print ("New training and test dataset.")
     pt_test=None
-    pbar=tqdm.tqdm(config.data_model.data_generator)
-    test_elements=defaultdict(list)
+    pbar=tqdm.tqdm(config.data_model_train.data_generator)
+    count=0
     for data in pbar:
-        pt, angles, features, scalar_features, weights, truth = config.data_model.getEvents(data)
-        batch_size = pt.shape[0]
-        train_size = int(batch_size*0.8)
-        train_mask = (torch.cat([torch.ones(train_size), torch.zeros(batch_size-train_size)],axis=0) == 1)
+        if count > 250: break
+        count=count+1
+        pt, angles, features, scalar_features, weights, truth = config.data_model_train.getEvents(data)
 
         optimizer.zero_grad()
 
         out  = config.model(
-            pt=pt[train_mask], 
-            angles=angles[train_mask], 
-            features=features[train_mask] if features is not None else None,
-            scalar_features=scalar_features[train_mask] if scalar_features is not None else None,)
+            pt=pt, 
+            angles=angles, 
+            features=features if features is not None else None,
+            scalar_features=scalar_features if scalar_features is not None else None,)
+        mask_nan = torch.isnan( out[:,0] ) 
+        loss = config.loss(out, truth, weights if weights is not None else None)
 
-        loss = config.loss(out, truth[train_mask], weights[train_mask] if weights is not None else None)
         pbar.set_description(f'Loss: {loss.item()}')
 
         n_samples = len(out)
         loss.backward()
         optimizer.step()
 
-        test_elements['pt'].append(pt[~train_mask])
-        test_elements['angles'].append(angles[~train_mask])
-        test_elements['scalar_features'].append(scalar_features[~train_mask] if scalar_features is not None else None)
-        test_elements['features'].append(features[~train_mask] if features is not None else None)
-        test_elements['weights'].append(weights[~train_mask])
-        test_elements['truth'].append(truth[~train_mask])
-    
-    for el in test_elements:
-        test_elements[el]=torch.cat( test_elements[el],axis=0) if test_elements[el][0] is not None else None
-            
-    with torch.no_grad():
-        out_test  =  config.model(
-            pt=test_elements['pt'], 
-            angles=test_elements['angles'], 
-            features=test_elements['features'],
-            scalar_features=test_elements['scalar_features'])
-        loss_test = config.loss( out_test, test_elements['truth'], test_elements['weights'])
-        print(f"Test loss is {loss_test}")
-        config.plot( out_test, test_elements['truth'], test_elements['weights'], model_directory, epoch)
-        if not "test_losses" in config.model.cfg_dict:
-            config.model.cfg_dict["train_losses"] = []
-            config.model.cfg_dict["test_losses"] = []
-        config.model.cfg_dict["train_losses"].append( loss.item() )
-        config.model.cfg_dict["test_losses"].append(  loss_test.item() )
 
-    print(f'Epoch {epoch:03d}: Loss(train): {loss:.4f} Loss(test): {loss_test:.4f}')
 
-    config.model.cfg_dict['epoch']       = epoch
+
     if args.prefix:
-        if loss_test.item()<config.model.cfg_dict['best_loss_test']:
-            config.model.cfg_dict['best_loss_test'] = loss_test.item()
-            torch.save(  config.model.state_dict(),     os.path.join( model_directory, 'best_state.pt'))
-            torch.save(  optimizer.state_dict(), os.path.join( model_directory, 'best_optimizer.pt'))
-            pickle.dump( config.model.cfg_dict,          open(os.path.join( model_directory, 'best_cfg_dict.pkl'),'wb'))
-            
         torch.save(  config.model.state_dict(),     os.path.join( model_directory, 'epoch-%d_state.pt' % epoch))
         torch.save(  optimizer.state_dict(), os.path.join( model_directory, 'epoch-%d_optimizer.pt' % epoch))
         pickle.dump( config.model.cfg_dict,          open(os.path.join( model_directory, 'epoch-%d_cfg_dict.pkl' % epoch),'wb'))
+    
+            
+    with torch.no_grad():
+        pbar=tqdm.tqdm(enumerate(config.data_model_test.data_generator))
+        chunks=[]
+        count=0
+        for chunk_number,data in pbar:
+            count = count+1
+            pt, angles, features, scalar_features, weights, truth = config.data_model_test.getEvents(data)
+
+            test_out  =  config.model(
+                pt=pt, 
+                angles=angles, 
+                features=features,
+                scalar_features=scalar_features)
+
+            chunks.append( config.plot_chunk( test_out, truth, weights,  scalar_features, chunk_number ))
+
+        merged_chunks=config.add_chunks( chunks ) 
+        config.save( merged_chunks, model_directory, epoch ) 
+    print(f'Epoch {epoch:03d}: Loss(train): {loss:.4f} ')
+
+            
